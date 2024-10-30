38a1db80a6103ad097c66f807473425d380ba4db
Gytis Ivaskevicius <me@gytis.io>
2024-10-30 11:42:02 +0200

    chore: expose nix example

--- oci/default.nix
+++ oci/default.nix
@@ -10,10 +10,17 @@
     }:
     let
       n2c = inputs.nix2container.packages.${system};
+      examples = import ./examples.nix {
+        inherit pkgs;
+        builder = self'.legacyPackages.oci.builder;
+      };
     in
     rec {
       legacyPackages.oci = {
         builder = import ./lib/builder.nix { inherit pkgs n2c; };
+        images = {
+          inherit (examples) nix;
+        };
         docs =
           (pkgs.nixosOptionsDoc {
             warningsAreErrors = false;
